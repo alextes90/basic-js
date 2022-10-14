3f18604d98f7d2eafe8b96b7a42b410c2e1e8f96
Aleksandr <a.teslin90@gmail.com>
2022-10-14 12:44:34 +0300

    feat: solve some more tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,61 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+    if (!(arr instanceof Array)) {
+        throw new Error("'arr' parameter must be an instance of the Array!");
+    }
+
+    const resultArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "--discard-prev") {
+            if (arr[i - 2] === "--discard-next") {
+                continue;
+            }
+            resultArr.pop();
+        } else if (arr[i] === "--discard-next") {
+            i++;
+        } else if (arr[i] === "--double-next") {
+            resultArr.push(arr[i + 1]);
+        } else if (arr[i] === "--double-prev") {
+            if (arr[i - 2] === "--discard-next") {
+                continue;
+            }
+            resultArr.push(arr[i - 1]);
+        } else {
+            resultArr.push(arr[i]);
+        }
+    }
+    const finalArr = [];
+    for (let el of resultArr) {
+        if (
+            el === "--double-prev" ||
+            el === "--discard-prev" ||
+            el === "--double-next" ||
+            el === "--discard-next" ||
+            !el
+        ) {
+            continue;
+        }
+
+        finalArr.push(el);
+    }
+
+    return finalArr;
 }
 
 module.exports = {
-  transform
+    transform,
 };
